2dcd4efc4665232f0cb0e87cad99f09f4abf8c82
AH-SHA <email.sa.coder@gmail.com>
2025-02-21 12:24:48 -0500

    Add files via upload
    Initial Commit; VSS would not push to GitHub

---
+++ Classes-Exercise/Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car firstCar = new Car();
+
+            firstCar.make = "Lexus";
+            firstCar.model = "ES";
+            firstCar.year = 2025;
+
+
+            Console.WriteLine(firstCar.year + " " + firstCar.make + " " + firstCar.model);
+
+        }
+    }
+}
